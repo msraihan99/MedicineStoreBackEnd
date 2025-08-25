1860d96db2392f6e1e786ca4fff20741205f1c08
Raihan <m.raihan.au23@gmail.com>
2025-08-25 17:12:20 +0530

    initial Commit

--- medicineStoreManagement/src/main/java/com/store/service/UserService.java
+++ medicineStoreManagement/src/main/java/com/store/service/UserService.java
@@ -5,6 +5,7 @@ import com.store.model.User;
 public interface UserService {
 
 	User saveUser(User user);
-	
-	 User login(String email, String rawPassword);
+
+	User login(String email, String rawPassword);
+
 }
